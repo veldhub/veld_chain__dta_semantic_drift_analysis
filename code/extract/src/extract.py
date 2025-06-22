5164d8fc7856d24aac689173555fca5f3883a792
SteffRhes <steffrhes@pm.me>
2025-06-22 14:12:29 +0200

    veld_step_2_extract: implement sampling

--- code/extract/src/extract.py
+++ code/extract/src/extract.py
@@ -72,6 +72,8 @@ def persist_text(path_txt, text):
 
 
 def main():
+
+    # produce output
     delete_output()
     for file in os.listdir(PATH_INPUT):
         path_xml = PATH_INPUT + file
@@ -84,5 +86,17 @@ def main():
                 path_txt = PATH_OUTPUT + decade + ".txt"
                 persist_text(path_txt, text)
 
+    # remove all but samples
+    sample_set = os.getenv("sample_set")
+    if sample_set is not None:
+        sample_set = [s + ".txt" for s in sample_set.split(",")]
+        for file in os.listdir(PATH_OUTPUT):
+            if file not in sample_set:
+                file_path = PATH_OUTPUT + file
+                print("removing:", file_path)
+                os.remove(file_path)
 
-main()
+
+if __name__ == "__main__":
+    main()
+
